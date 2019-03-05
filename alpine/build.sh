2a977b4f641a77fee63c89602701683eacb2b8a6
feifeigd <feifeigd@21cn.com>
2019-03-05 17:04:57 +0800

    clang 继承gcc

--- alpine/build.sh
+++ alpine/build.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env sh
 
 # 有些有依赖关系的，不能按照字母顺序构建
-docker build -t alpine-base			base
-docker build -t clang-alpine		clang
-docker build -t ssh-alpine			ssh  
-docker build -t gcc-alpine			gcc
-docker build -t nginx-alpine		nginx
-docker build -t php-alpine		    php
-docker build -t redis-alpine		redis
-docker build -t shadowsocks-alpine	shadowsocks
-docker build -t skynet-alpine		skynet
-docker build -t svn-alpine			svn
-docker build -t thinkphp 			thinkphp
+docker image build -t alpine-base        base
+docker image build -t ssh-alpine         ssh  
+docker image build -t gcc-alpine         gcc
+docker image build -t clang-alpine       clang
+docker image build -t nginx-alpine       nginx
+docker image build -t php-alpine         php
+docker image build -t redis-alpine       redis
+docker image build -t shadowsocks-alpine shadowsocks
+docker image build -t skynet-alpine      skynet
+docker image build -t svn-alpine         svn
+docker image build -t thinkphp           thinkphp
