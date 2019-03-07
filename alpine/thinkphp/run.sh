97c5eec8be621dcc62a0b439b98bfad2a1dae7c5
feifeigd <feifeigd@21cn.com>
2019-03-07 12:10:41 +0800

    更新版本

--- alpine/thinkphp/run.sh
+++ alpine/thinkphp/run.sh
@@ -4,5 +4,5 @@ CONF_FILE=/etc/nginx/nginx.conf
 
 sed -i "s/#LISTEN_PORT#/${LISTEN_PORT}/" ${CONF_FILE}
 
-php-fpm5 -d variables_order="EGPCS" && exec nginx -g "daemon off;"
+php-fpm7 -d variables_order="EGPCS" && exec nginx -g "daemon off;"
 
